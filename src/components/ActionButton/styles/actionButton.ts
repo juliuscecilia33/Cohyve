fdea96ac0ece2b40f449762a4a39d067822042b4
Julius <juliuscecilia33@gmail.com>
2021-11-05 09:12:48 -0700

    fixed navbar

--- src/components/ActionButton/styles/actionButton.ts
+++ src/components/ActionButton/styles/actionButton.ts
@@ -1,14 +1,14 @@
 import styled from "styled-components/macro";
 
 export const Container = styled.button`
-  height: 56px;
-  padding: 0 1.5rem;
+  height: 42px;
+  padding: 0 2rem;
   background: linear-gradient(94.39deg, #58a4b0 8.09%, #afd5aa 93.12%), #284b63;
   border-radius: 100px;
   cursor: pointer;
   font-family: Poppins;
   font-style: normal;
   font-weight: 600;
-  font-size: 22px;
+  font-size: 16px;
   color: #f0f2ef;
 `;
--- src/components/Navbar/styles/navbar.ts
+++ src/components/Navbar/styles/navbar.ts
@@ -2,7 +2,7 @@ import styled from "styled-components/macro";
 
 export const Container = styled.div`
   width: 100%;
-  height: 10vh;
+  height: 9vh;
   background: #fafcfa;
   display: flex;
   justify-content: space-between;
@@ -17,14 +17,15 @@ export const ContainerMed = styled.div`
 `;
 
 export const Logo = styled.img`
-  height: 45px;
+  height: 25px;
+  cursor: pointer;
   width: auto;
 `;
 
 export const Border = styled.div`
-  border-left: 1px solid red;
-  height: 100%;
-  margin: 0 1rem;
+  border-left: 1.5px solid #dadada;
+  height: 60%;
+  margin: 2rem;
 `;
 
 export const Tabs = styled.div`
@@ -37,6 +38,7 @@ export const Tab = styled.h2`
   font-family: Poppins;
   font-style: normal;
   font-weight: bold;
-  font-size: 26px;
+  font-size: 20px;
   color: #284b63;
+  margin-right: 4rem;
 `;
