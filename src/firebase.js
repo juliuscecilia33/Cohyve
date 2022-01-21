bc84b328d9f93ce7809c882a42f93d16349ffb83
Julius <juliuscecilia33@gmail.com>
2022-01-21 10:52:56 -0800

    changed to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -9,11 +9,11 @@ import {
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "cohyve.firebaseapp.com",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: "cohyve",
   storageBucket: "cohyve.appspot.com",
-  messagingSenderId: "814912928639",
-  appId: "1:814912928639:web:b81586702f59bd116c384a",
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
