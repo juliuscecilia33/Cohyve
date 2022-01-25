b26deb4037773e05963c57130e1697cee21bdeb3
Julius <juliuscecilia33@gmail.com>
2022-01-25 09:06:40 -0800

    dropdown for club and school

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 
-import { Container, Dropdown, Icon } from "./styles/search";
+import {
+  Container,
+  Dropdown,
+  Icon,
+  DropdownItems,
+  DropdownClubItem,
+  DropdownSchoolItem,
+} from "./styles/search";
+
+import EnterIcon from "../../../public/images/EnterIcon.png";
 
 type Props = {
   name?: string;
   type?: string;
   placeholder?: string;
   onChange?: any;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   value?: any;
+  showItems?: boolean;
+  clubProfileImage?: any;
+  clubName?: string;
+  clubSchool?: string;
+  clubState?: string;
   children?: React.ReactNode;
+  schoolName?: string;
 };
 
 export default function Search({
@@ -28,7 +44,6 @@ export default function Search({
         onChange={onChange}
         value={value}
         placeholder={placeholder}
-        required
       />
       <Search.Icon />
     </Container>
@@ -45,4 +60,57 @@ Search.Icon = function SearchIcon({ children, ...restProps }: Props) {
 
 Search.Dropdown = function SearchDropdown({ children, ...restProps }: Props) {
   return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Search.DropdownItems = function SearchDropdownItems({
+  showItems,
+  children,
+  ...restProps
+}: Props) {
+  return (
+    <DropdownItems showItems={showItems} {...restProps}>
+      {children}
+    </DropdownItems>
+  );
+};
+
+Search.DropdownClubItem = function SearchDropdownClubItem({
+  clubProfileImage,
+  clubName,
+  clubSchool,
+  clubState,
+  onClick,
+  children,
+  ...restProps
+}: Props) {
+  return (
+    <DropdownClubItem onClick={onClick} {...restProps}>
+      <img src={clubProfileImage} alt={clubName} />
+      <h3>{clubName}</h3>
+      <p>
+        - {clubSchool}, {clubState}
+      </p>
+      <div>
+        <p>Enter</p>
+        <img src={EnterIcon} alt="Enter" />
+      </div>
+    </DropdownClubItem>
+  );
+};
+
+Search.DropdownSchoolItem = function SearchDropdownSchoolItem({
+  schoolName,
+  onClick,
+  children,
+  ...restProps
+}: Props) {
+  return (
+    <DropdownSchoolItem onClick={onClick} {...restProps}>
+      <h3>{schoolName}</h3>
+      <div>
+        <p>Enter</p>
+        <img src={EnterIcon} alt="Enter" />
+      </div>
+    </DropdownSchoolItem>
+  );
 };
