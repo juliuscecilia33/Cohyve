f23cd799a9671c58dff688267f0b7fb825e1f82f
Julius <juliuscecilia33@gmail.com>
2022-01-26 09:27:22 -0800

    creating filter component

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,15 +1,87 @@
 import React from "react";
 
-import { Container } from "./styles/filter";
+import {
+  Container,
+  Block,
+  Input,
+  Dropdown,
+  DropdownItem,
+} from "./styles/filter";
 
 type Props = {
   children?: React.ReactNode;
+  horizontal?: boolean;
+  title?: string;
 };
 
-export default function Filter({ children, ...restProps }: Props) {
-  return <Container {...restProps}>{children}</Container>;
+type InputProps = {
+  onChange?: React.FormEventHandler<HTMLDivElement>;
+  name?: string;
+  type?: string;
+  placeholder?: string;
+  value?: any;
+  children?: React.ReactNode;
+};
+
+type ItemProps = {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  schoolName?: string;
+  children?: React.ReactNode;
+};
+
+export default function Filter({ horizontal, children, ...restProps }: Props) {
+  return (
+    <Container horizontal={horizontal} {...restProps}>
+      {children}
+    </Container>
+  );
 }
 
-Filter.Members = function FilterMembers({ children, ...restProps }: Props) {
-  return <Members {...restProps}>{children}</Members>;
+Filter.Block = function FilterBlock({ title, children, ...restProps }: Props) {
+  return (
+    <Block {...restProps}>
+      <h3>{title}</h3>
+      {children}
+    </Block>
+  );
+};
+
+Filter.Input = function FilterInput({
+  name,
+  type,
+  onChange,
+  value,
+  placeholder,
+  children,
+  ...restProps
+}: InputProps) {
+  return (
+    <Input {...restProps}>
+      <input
+        name={name}
+        type={type}
+        onChange={onChange}
+        value={value}
+        placeholder={placeholder}
+      />
+      <i className="fas fa-chevron-down"></i>
+    </Input>
+  );
+};
+
+Filter.Dropdown = function FilterDropdown({ children, ...restProps }: Props) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Filter.DropdownItem = function FilterDropdownItem({
+  onClick,
+  schoolName,
+  children,
+  ...restProps
+}: ItemProps) {
+  return (
+    <DropdownItem onClick={onClick} {...restProps}>
+      {children} <h3>{schoolName}</h3>
+    </DropdownItem>
+  );
 };
